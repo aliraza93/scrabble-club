80c1b508c1a2d7264fff712d48baf8c740ffc9b1
Ali Raza <55763702+aliraza93@users.noreply.github.com>
2024-06-07 20:49:54 +0500

    Define eloquent relationships

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Member extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'email', 'phone', 'joined_at'];
+
+    public function games()
+    {
+        return $this->belongsToMany(Game::class)->withPivot('score')->withTimestamps();
+    }
 }
